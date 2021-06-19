87b1cfc40decefbe8ddddfbf6fa980819ac594bc
Hongrui-Ji <hongruiji0515@gmail.com>
2021-06-19 23:15:52 +0800

    demo3

---
+++ demo3/pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<view>
+
+		<button type="primary" @click="goto" style="margin: 20rpx;">打开手写板</button>
+		<view style="margin:30rpx 20rpx 20rpx;font-size: 36rpx;">结果 ：</view>
+		<image :src="resultUrl" style="width: 100%;" mode="widthFix"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				resultUrl: '',
+			}
+		},
+		mounted() {
+			this.$nextTick(function() {});
+		},
+		onShow() {
+			try {
+				if (uni.getStorageSync('ceshiUrl')) {
+					this.resultUrl = uni.getStorageSync('ceshiUrl');
+					uni.removeStorageSync('ceshiUrl');
+				}
+
+			} catch (e) {
+				// error
+			}
+		},
+		methods: {
+			goto() {
+				uni.navigateTo({
+					url: '../signature/signature'
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ demo3/pages/signature/signature.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+		<htz-signature @sumbit="sumbit"  @fail="fail" cid="ceshi1"></htz-signature>
+	</view>
+</template>
+
+<script>
+	import htzSignature from '@/components/htz-signature/htz-signature.vue'
+	export default {
+		data() {
+			return {
+			}
+		},
+		components: {
+			htzSignature,
+		},
+		mounted() {
+			this.$nextTick(function() {
+			});
+
+		},
+		methods: {	
+			sumbit(res){
+				console.log('sumbit',res)
+				try {
+				    uni.setStorageSync('ceshiUrl', res.tempFilePath);
+					uni.navigateBack();
+				} catch (e) {
+				    // error
+				}
+			},
+			fail(err){
+				console.log('fail',err)
+			},
+		}
+	}
+</script>
+
+<style>
+
+</style>
